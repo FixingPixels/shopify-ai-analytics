0147785823d226ef1769748ccce8d47e83bd6b1e
aminakm123 <shamrifari@gmail.com>
2024-10-11 21:08:21 +0530

    db changed to postgresql

--- ai_shopify_dashboard/ai_shopify_dashboard/settings.py
+++ ai_shopify_dashboard/ai_shopify_dashboard/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = "ai_shopify_dashboard.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ai_shopify_analytics', 
+        'USER': 'ai_shopify',
+        'PASSWORD': 'ai_shopify@123', 
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
